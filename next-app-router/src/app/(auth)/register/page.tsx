715ef326b691192e88e4a409f8e3795309885658
TirtaSuci <tirtas43@gmail.com>
2025-05-22 15:10:11 +0700

    update

---
+++ next-app-router/src/app/(auth)/register/page.tsx
@@ -0,0 +1,5 @@
+export default function ResgisterPage() {
+    return (
+        <p>Resgister Page</p>
+    )
+}
---
+++ next-app-router/src/app/product/page.tsx
@@ -0,0 +1,5 @@
+export default function ProductPage() {
+    return (
+        <p>Product Page</p>
+    )
+}
