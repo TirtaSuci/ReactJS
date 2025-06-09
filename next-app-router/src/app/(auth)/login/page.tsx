a149805d8ea054e793a5ff552607caceec873d42
TirtaSuci <tirtas43@gmail.com>
2025-06-09 10:52:00 +0700

    update

--- next-app-router/src/app/(auth)/login/page.tsx
+++ next-app-router/src/app/(auth)/login/page.tsx
@@ -37,7 +37,7 @@ export default function LoginPage() {
                             type="password"
                             name="password"
                             id="password"
-                            placeholder="••••••••"
+                            placeholder="•••••••"
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                             required
                         />
