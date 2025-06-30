20ddb5a5c1c1ea50045bfb9cc9b2ea6aaed328a7
TirtaSuci <tirtas43@gmail.com>
2025-06-30 14:52:14 +0700

    update

--- next-app-router/src/app/product/page.tsx
+++ next-app-router/src/app/product/page.tsx
@@ -1,4 +1,3 @@
-
 import { getData } from "@/app/services/products";
 import Link from "next/link";
 
@@ -12,25 +11,22 @@ export default async function ProductPage(props: ProductPageProps) {
             {/* <h1>{params.slug ? "Detail Product Page " : "Product Page"}</h1> */}
             {products.length > 0 &&
                 products.map((product: any) => (
-                    <Link href={`/product/detail/${product.id}`}
+                    <Link
+                        href={`/product/detail/${product.id}`}
                         key={product.id}
                         className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
                     >
-                        <div>
-                            <img
-                                className="p-8 rounded-t-lg object-cover h-50 w-full"
-                                src={product.image}
-                                alt="product image"
-                                width={500}
-                                height={500}
-                            />
-                        </div>
-                        <div className="px-5 pb-5">
-                            <div>
-                                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
-                                    {product.title}
-                                </h5>
-                            </div>
+                        <img
+                            className="p-4 border-2 rounded-t-lg object-contain h-70 w-full bg-white"
+                            src={product.image}
+                            alt="product image"
+                            width={500}
+                            height={500}
+                        />
+                        <div className="px-5 pb-5 pt-5">
+                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
+                                {product.title}
+                            </h5>
                             <div className="flex items-center mt-2.5 mb-5">
                                 <div className="flex items-center space-x-1 rtl:space-x-reverse">
                                     <svg
@@ -87,11 +83,12 @@ export default async function ProductPage(props: ProductPageProps) {
                                 <span className="text-xl font-bold text-gray-900 dark:text-white">
                                     ${product.price}
                                 </span>
-                                <div
+                                <button
+                                    type="button"
                                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                 >
                                     Add to cart
-                                </div>
+                                </button>
                             </div>
                         </div>
                     </Link>
