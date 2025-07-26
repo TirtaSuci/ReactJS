166b3cbbe5d00793404b0450d6f963b4a68c7686
TirtaSuci <tirtas43@gmail.com>
2025-07-26 16:49:33 +0700

    update

--- next-app-router/src/app/components/core/Modal/page.tsx
+++ next-app-router/src/app/components/core/Modal/page.tsx
@@ -1,27 +1,27 @@
-'use client'
+// 'use client'
 
-import { useRouter } from 'next/navigation'
-import { MouseEventHandler, useRef } from "react";
+// import { useRouter } from 'next/navigation'
+// import { MouseEventHandler, useRef } from "react";
 
-export default function Modal({ children }: { children: React.ReactNode }) {
-    const overlay = useRef(null);
-    const router = useRouter();
+// export default function Modal({ children }: { children: React.ReactNode }) {
+//     const overlay = useRef(null);
+//     const router = useRouter();
 
-    const close: MouseEventHandler = (e) => {
-        if (e.target === overlay.current) {
-            router.back();
-        }
-    };
+//     const close: MouseEventHandler = (e) => {
+//         if (e.target === overlay.current) {
+//             router.back();
+//         }
+//     };
 
-    return (
-        <div
-            ref={overlay}
-            onClick={close}
-            className="fixed z-10 left-0 right-0 top-0 bottom-0 max-auto bg-black/60"
-        >
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                {children}
-            </div>
-        </div>
-    );
-}
+//     return (
+//         <div
+//             ref={overlay}
+//             onClick={close}
+//             className="fixed z-10 left-0 right-0 top-0 bottom-0 max-auto bg-black/60"
+//         >
+//             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+//                 {children}
+//             </div>
+//         </div>
+//     );
+// }
