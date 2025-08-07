4bb236754dd0168f2b991ce10884012fc439c48d
TirtaSuci <tirtas43@gmail.com>
2025-08-07 11:32:11 +0700

    update

--- next-app-router/src/app/product/@modal/(.)detail/[id]/page.tsx
+++ next-app-router/src/app/product/@modal/(.)detail/[id]/page.tsx
@@ -37,12 +37,12 @@ export default async function DetailProductPage({ params }: ProductPageProps) {
         // </Modal>
 
         <Modal>
-            <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-3xl md:h-[400px] p-6 dark:border-gray-700 dark:bg-gray-800">
                 <Image
-                    className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                    className="object-cover w-full rounded-lg h-96 md:h-auto md:w-80 bg-white p-4"
                     src={data.image}
                     alt=""
-                    width={800}
+                    width={500}
                     height={500}
                 />
                 <div className="flex flex-col justify-between p-4 leading-normal">
@@ -52,6 +52,10 @@ export default async function DetailProductPage({ params }: ProductPageProps) {
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                         {data.description}
                     </p>
+                    <div className="flex items-center justify-between">
+                        <p className="text-2xl font-bold text-gray-900 dark:text-white">${data.price}</p>
+                        <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">add to chart</button>
+                    </div>
                 </div>
             </div>
         </Modal>
